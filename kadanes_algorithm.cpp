cfa79e2d6c8e55e4a3e7d38f9ccc167668bb7a98
knowthyse1f <knowtobuildthyself@gmail.con>
2024-03-21 16:33:50 +0600

    safu

--- kadanes_algorithm.cpp
+++ kadanes_algorithm.cpp
@@ -1,12 +1,17 @@
-//kadane's algorithm 
+//kadane's algorithm
+//time complexity O(n) 
 #include<bits/stdc++.h>
 using namespace std;
 
 int maximum_subarray_sum(int a[], int n){
-
-
-
-
+     int cs=0;
+     int largest=0;
+     for(int i=0;i<n;i++){
+        cs+=a[i];
+        if(cs<0)cs=0;
+        if(cs>largest)largest=cs;
+     }
+     return largest;
 
 }
 
@@ -15,7 +20,7 @@ int main(){
     int a[]={4,-5,9,-6,1,-2,3};
     int n= sizeof(a)/sizeof(int);
 
-    maximum_subarray_sum(a,n);
+    cout<<maximum_subarray_sum(a,n)<<endl;
 
     return 0;
 }
