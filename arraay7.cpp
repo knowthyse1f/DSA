1dba92bb107111f28e7ff35d32576b0e858c4eb9
knowthyse1f <knowtobuildthyself@gmail.con>
2024-03-21 14:40:20 +0600

    safu

--- arraay7.cpp
+++ arraay7.cpp
@@ -1,18 +1,34 @@
 //print all sub array 
+//find sum of all sub array (bruteforce approch ) 
 
 #include<bits/stdc++.h>
 using namespace std;
 
 void printSubarray(int a[],int n){
-
+    int maxsum=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    sum=sum+a[k];
+                    cout<<a[k]<<", ";
+                }
+                cout<<endl;
+                if(sum>maxsum)maxsum=sum;
+               // cout<<"sum : "<<sum<<endl;
+            }
+            
+        }
+        cout<<"maxsum: "<<maxsum<<endl;
 }
 
 
 int main(){
 
-    int a[]={4,5,9,6,1,2,3};
+    int a[]={4,-5,9,-6,1,-2,3};
     int n= sizeof(a)/sizeof(int);
 
+    printSubarray(a,n);
 
 return 0;
 }
